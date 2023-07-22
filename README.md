da26cc942bd1ea43e50ee744c8ec35fcba82e93a
Mark Skelton <mdskelton99@gmail.com>
2023-07-22 07:38:03 -0500

    Add release and build scripts

--- README.md
+++ README.md
@@ -4,8 +4,19 @@ Parse URLs from a string.
 
 ## Installation
 
+You can install url by running the install script which will download
+the [latest release](https://github.com/mskelton/url/releases/latest).
+
 ```bash
-go install github.com/mskelton/url@latest
+curl -LSfs https://mskelton.dev/url/install | sh
+```
+
+Or you can build from source.
+
+```bash
+git clone git@github.com:mskelton/url.git
+cd url
+go install .
 ```
 
 ## Usage
---
+++ scripts/build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -eu
+
+package=url
+platforms=(
+	"windows/amd64"
+	"windows/arm64"
+	"darwin/amd64"
+	"darwin/arm64"
+	"linux/amd64"
+	"linux/arm64"
+)
+
+for platform in "${platforms[@]}"; do
+	echo "Building for $platform"
+
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+
+	bin=$package
+	if [ $GOOS = "windows" ]; then
+		bin+='.exe'
+	fi
+
+	# Build the binary
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o $bin
+
+	# Zip the binary
+	mkdir -p dist
+	output_name="$package-$GOOS-$GOARCH"
+
+	if [ $GOOS = "windows" ]; then
+		output_name="$output_name.zip"
+		7z a dist/$output_name $bin
+	else
+		output_name="$output_name.tar.gz"
+		tar czf dist/$output_name $bin
+	fi
+
+	if [ $? -ne 0 ]; then
+		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+done
